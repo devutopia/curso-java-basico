9e0b9e22c07319737b94aa58c6fbc8663d931a8a
devutopia <holy_lima@yahoo.com.br>
2017-07-27 08:58:50 -0300

    Add aula11

---
+++ src/com/kleber/javabasico/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,16 @@
+package com.kleber.javabasico.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+		
+		double valorDaPassagem = 2.90;
+		
+		float valorTomate = 3.50f;
+		
+		System.out.println("Valor da passagem = " + valorDaPassagem);
+		System.out.println("Valor do tomate = " + valorTomate);
+
+	}
+
+}
