7797bd6b11ca1d2665a6f71ad5b49297132c8cb9
Kleber Lima <Kleber Lima>
2017-08-02 01:22:59 -0300

    add aula 14

---
+++ src/com/kleber/javabasico/aula14/IfElse.java
@@ -0,0 +1,38 @@
+package com.kleber.javabasico.aula14;
+
+import java.util.Scanner;
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite sua idade.");
+		int idade = scan.nextInt();
+		
+		if (idade >= 18){
+			System.out.println("Você é maior de idade!");
+		} else {
+			System.out.println("Você é menor de idade!");
+		}
+		
+		System.out.println("Entre com o preço do item");
+		double valor = scan.nextDouble();
+		
+		
+		//ifs aninhados
+		if(valor <= 10){
+			System.out.println("Está barato, pode comprar");
+		} else if (valor > 10 && valor < 15){
+			System.out.println("Você pode pedir um desconto");
+		} else if (valor >= 15 && valor < 17){
+			System.out.println("Pode pesquisar mais");
+		} else {
+			System.out.println("Está muito caro!");
+		}
+				
+
+	}
+
+}
