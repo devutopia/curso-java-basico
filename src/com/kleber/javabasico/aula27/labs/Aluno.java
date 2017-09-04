54eb02c8ceeb7bd6dc7ea5905ed23eedbcbf9b4e
devutopia <holy_lima@yahoo.com.br>
2017-09-04 00:25:14 -0300

    add class Exer03

--- src/com/kleber/javabasico/aula27/labs/Aluno.java
+++ src/com/kleber/javabasico/aula27/labs/Aluno.java
@@ -3,8 +3,28 @@ package com.kleber.javabasico.aula27.labs;
 public class Aluno {
 	
 	String nome;
+	String matricula;
+	String nomeCurso;
+	String[] nomeDisciplinas;
+	double[][] notasDisciplinas;
 	
+	void mostrarInfo(){
+		System.out.println("Nome: " + nome);
+		System.out.println("Matrícula: " + matricula);
+		System.out.println("Curso: " + nomeCurso);
+		
+		for (int i=0; i < notasDisciplinas.length; i++){
+			System.out.println("Notas da disciplina " + nomeDisciplinas[i]);
+			for (int j=0; j < notasDisciplinas[i].length; j++){
+				System.out.println(notasDisciplinas[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
 	
+	void verificarAprovado (){
+		
+	}
 	
 
 }
---
+++ src/com/kleber/javabasico/aula27/labs/Exer03.java
@@ -0,0 +1,43 @@
+package com.kleber.javabasico.aula27.labs;
+
+import java.util.Scanner;
+
+public class Exer03 {
+
+	private static Scanner scan;
+
+	public static void main(String[] args) {
+		
+		scan = new Scanner(System.in);
+		
+		Aluno aluno = new Aluno();
+		
+		System.out.println("Entre com o nome do aluno");
+		aluno.nome = scan.next();
+		
+		System.out.println("Entre com o nome do curso");
+		aluno.nomeCurso = scan.next();
+		
+		System.out.println("Entre com a matricula");
+		aluno.matricula = scan.next();
+		
+		
+		aluno.nomeDisciplinas = new String[3];
+		for (int i = 0; i < aluno.nomeDisciplinas.length; i++){
+			System.out.println("Entre com o nome da disciplina" + (i+1));
+			aluno.nomeDisciplinas[i] = scan.next();
+		}
+		aluno.notasDisciplinas = new double[3][4];
+		for (int i=0; i < aluno.notasDisciplinas.length; i++){
+			System.out.println("Obtendo notas da disciplina " + aluno.nomeDisciplinas[i]);
+			for (int j=0; j < aluno.notasDisciplinas[i].length; j++){
+				System.out.println("Entre com a nota " + (j+1));
+				aluno.notasDisciplinas[i][j] = scan.nextDouble();
+			}
+		}
+		
+		aluno.mostrarInfo();
+
+	}
+
+}
