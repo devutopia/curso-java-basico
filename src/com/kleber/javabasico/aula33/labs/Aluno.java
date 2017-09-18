d994ebb7984793e45117e0981bfb539c736eb1f1
devutopia <holy_lima@yahoo.com.br>
2017-09-17 21:01:55 -0300

    add class aluno in package labs33

--- src/com/kleber/javabasico/aula33/labs/Aluno.java
+++ src/com/kleber/javabasico/aula33/labs/Aluno.java
@@ -7,5 +7,35 @@ public class Aluno {
 	private String curso;
 	private String[] disciplinas;
 	private double[] notas;
+	public String getNomeAluno() {
+		return nomeAluno;
+	}
+	public void setNomeAluno(String nomeAluno) {
+		this.nomeAluno = nomeAluno;
+	}
+	public String getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public String[] getDisciplinas() {
+		return disciplinas;
+	}
+	public void setDisciplinas(String[] disciplinas) {
+		this.disciplinas = disciplinas;
+	}
+	public double[] getNotas() {
+		return notas;
+	}
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
 
 }
