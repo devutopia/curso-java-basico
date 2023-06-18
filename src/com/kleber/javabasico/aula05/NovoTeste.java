e14c7cca9d45ddc11b8108b41f3b88c40d699f98
klimapires <kleber-jobs@outlook.com>
2023-06-18 01:15:32 -0300

    Testing new IDE

---
+++ src/com/kleber/javabasico/aula05/NovoTeste.java
@@ -0,0 +1,7 @@
+package com.kleber.javabasico.aula05;
+
+public class NovoTeste {
+  public static void main(String[] args) {
+    System.out.println("Novo teste a partir de um projeto executado no Eclipese em VSCode");
+  }
+}
